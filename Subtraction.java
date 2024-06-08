548f88025c8a00f73347a84105e8d24daeb1b139
sridharanish19 <sridharanish19@gmail.com>
2024-06-08 13:01:10 -0500

    Create Subtraction.java

---
+++ Subtraction.java
@@ -0,0 +1,10 @@
+public class Subtraction {
+    public static void main(String[] args){
+        int a = 20;
+        int b = 10;
+        System.out.println("Number 1: "+a);
+        System.out.println("Number 2: "+b);
+        int result = a - b;
+        System.out.println("Addition of two numbers is: "+result);
+    }
+}
